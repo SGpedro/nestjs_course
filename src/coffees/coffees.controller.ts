05ff48d2cde04c6819eda96d5cf1a56bd2c77ce5
sgpedro <pedro.guemureman@hotmail.com>
2021-04-12 11:51:41 -0300

    several codes

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Get, Param, Patch, Post, Delete, Query} from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post, Delete, Query, UsePipes, ValidationPipe} from '@nestjs/common';
+import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
 import { CoffeesService } from './coffees.service'
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
@@ -9,9 +10,9 @@ export class CoffeesController {
     constructor(private readonly coffeesServices: CoffeesService){}
 
     @Get()
-    findAll(@Query() paginationQuery){
+    findAll(@Query() paginationQuery: PaginationQueryDto){
         // const { limit, offset } = paginationQuery;
-        return this.coffeesServices.findAll();
+        return this.coffeesServices.findAll(paginationQuery);
         // return `This action retuns all coffees. Limit ${limit}, Offset ${offset}`
     }
 
---
+++ src/migrations/1618003205372-CoffeeRefactor.ts
@@ -0,0 +1,19 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CoffeeRefactor1618003205372 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+
+        await queryRunner.query(
+            `ALTER TABLE "coffee" RENAME COLUMN "name" TO "title"`
+        );
+
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(
+            `ALTER TABLE "coffee" RENAME COLUMN "title" TO "name"`
+        );
+    }
+
+}
